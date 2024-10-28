79481b337d44882d138eb61ced8e96e60837a03e
Aimad Elkaouri <aimadelkaouri@gmail.com>
2024-10-28 23:50:33 +0100

    commit2

---
+++ script.js
@@ -0,0 +1,13 @@
+function scrollPage(){
+    window.scrollTo(0, 700);
+}
+
+function toabout(event){
+    event.preventDefault();
+    window.scrollTo(0, 700);
+}
+
+function toskills(event){
+    event.preventDefault();
+    window.scrollTo(0, 2190)
+}
